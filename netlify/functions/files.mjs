4ba3c1c88db5cbaad29556422a52d57ac9f02106
Astroclasse <astrorgu@gmail.com>
2026-08-10 10:50:05 +0200

    Delete netlify directory

--- netlify/functions/files.mjs
+++
@@ -1,5 +0,0 @@
-import { getStore } from "@netlify/blobs";
-
-const STORE = "revisapp-files";
-const ALLOWED = new Set([.doc", ".docx", ".pdf", ".txt", ".ppt", ".pptx", ".mp3"]);
-const MAX_SIZE = 5 * 1024 * 1024; // 5 
